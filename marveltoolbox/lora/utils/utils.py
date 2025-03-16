98ee02916d301abad25a1d5f750327d7869bc5aa
DawnKr <2168608696@qq.com>
2025-03-16 11:00:16 +0800

    fix lora bias parameter passing issue

--- marveltoolbox/lora/utils/utils.py
+++ marveltoolbox/lora/utils/utils.py
@@ -7,15 +7,15 @@ def lora_state_dict(model):
     return {key: state_dict[key] for key in state_dict.keys() if "lora" in key}
 
 
-def set_lora_configs_all(model, rank: int, alpha: int, enable_lora=True):
+def set_lora_configs_all(model, rank: int, alpha: int, bias=False, enable_lora=True):
     # Before calling this function, check the type of the input model
     if isinstance(model, LoRA):
         lora_layers = model.get_lora_layers()
         for layer in lora_layers:
-            layer.set_lora_configs(rank, alpha)
+            layer.set_lora_configs(rank, alpha, bias)
             layer.set_lora_status(enable_lora)
     elif isinstance(model, LoRALayer):
-        model.set_lora_configs(rank, alpha)
+        model.set_lora_configs(rank, alpha, bias)
         model.set_lora_status(enable_lora)
     else:
         raise ModelModificationError()
